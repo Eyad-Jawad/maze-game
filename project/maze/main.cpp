34724cc31a94cc223c50f03dd2d4e9b6cc047394
eyad-jawad <ayadmansr@gmail.com>
2026-04-12 17:27:39 +0300

    Make a WASM file and a JS file

---
+++ project/maze/main.cpp
@@ -0,0 +1,53 @@
+/*
+    Add logs.
+    check emscripten
+    --Eyad
+*/
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include "mazeGenerator.h"
+#include "mazeSolver.h"
+
+void savePPM(const std::vector <uint8_t> & maze, int side);
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: ./main.exe MazeDims\n";
+        return 1;
+    }
+
+    int N = std::stoi(argv[1]); // turn the arg of mazedims into an int
+    mazeGrid mazeGenerator = mazeGrid(N); // pass the dimensions
+    mazeGenerator.makeMaze();
+
+    pathFinder mazeSolver = pathFinder(mazeGenerator);
+    mazeSolver.solveMaze();
+
+    savePPM(mazeSolver.getMaze(), mazeSolver.getSide());
+    
+    return 0;
+}
+
+void savePPM(const std::vector <uint8_t> & maze, int side) {
+    std::ofstream mazeImage("./mazeImage.ppm", std::ios::binary);
+
+    mazeImage << "P6\n" << side << ' ' << side << "\n255\n";
+
+    for (auto const & cell : maze) {
+        uint8_t r, g, b;
+
+        if (cell == 0) {
+            r = g = b = 0;
+        } else if (cell == 1) {
+            r = g = b = 255;
+        } else {
+            r = b = 0;
+            g = 255;
+        }
+
+        mazeImage.write((char*)&r, 1);
+        mazeImage.write((char*)&g, 1);
+        mazeImage.write((char*)&b, 1);
+    }
+}
---
+++ project/maze/testing.cpp
@@ -0,0 +1,140 @@
+#include <gtest/gtest.h>
+#include "mazeGenerator.h"
+#include "mazeSolver.h"
+
+class testingMaze : public ::testing::TestWithParam<int> {};
+class testingInvalidMaze : public ::testing::TestWithParam<int> {};
+
+class testingMazeSolver : public ::testing::TestWithParam <int> {};
+
+TEST_P(testingMaze, testSide) {
+    int N = GetParam();
+    mazeGrid m(N);
+    EXPECT_EQ(m.getSide(), 2 * N + 1);
+}
+
+TEST_P(testingMaze, testSize) {
+    int N = GetParam();
+    mazeGrid m(N);
+    int side = 2 * N + 1;
+    EXPECT_EQ(m.getMazeSize(), side * side);
+}
+
+TEST_P(testingMaze, testDimensions) {
+    int N = GetParam();
+    mazeGrid m(N);
+    EXPECT_EQ(m.getDimensions(), N);
+}
+
+TEST(testingMaze, testMazeValues) {
+    mazeGrid m(2);
+    m.setSeed(0);
+    m.makeMaze();
+
+    std::vector <uint8_t> &maze = m.getMaze();
+    std::vector <uint8_t> mazeDimTwoSeedZero = {0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+
+    for(int i = 0, size = maze.size(); i < size; i++) {
+        EXPECT_EQ(maze[i], mazeDimTwoSeedZero[i]);
+    }
+}
+
+TEST_P(testingInvalidMaze, testInvalidDimensions) {
+    int invalidN = GetParam();
+    EXPECT_THROW(
+        {
+            mazeGrid m(invalidN);
+            m.makeMaze();
+        }, 
+        std::runtime_error
+    );
+}
+
+TEST_P(testingMazeSolver, testSolveability) {
+    int N = GetParam();
+    mazeGrid m(N);
+    m.makeMaze();
+
+    pathFinder s(m);
+    s.solveMaze();
+    std::vector <uint8_t> solvedMaze = s.getMaze();
+
+    EXPECT_FALSE(solvedMaze.empty());
+}
+
+TEST_P(testingMazeSolver, testSolveabilityExetreme) {
+    int N = GetParam();
+    for (int _ = 0; _ < 10; _++) {
+        mazeGrid m(N);
+        m.makeMaze();
+    
+        pathFinder s(m);
+        s.solveMaze();
+        std::vector <uint8_t> solvedMaze = s.getMaze();
+    
+        EXPECT_FALSE(solvedMaze.empty());
+    }
+}
+
+TEST_P(testingMazeSolver, testSide) {
+    int N = GetParam();
+    mazeGrid m(N);
+    m.makeMaze();
+    pathFinder s(m);
+
+    int side = 2 * N + 1;
+
+    EXPECT_EQ(s.getSide(), side);
+}
+
+TEST_P(testingMazeSolver, testGoal) {
+    int N = GetParam();
+    mazeGrid m(N);
+    m.makeMaze();
+
+    int side = 2 * N + 1;
+    pathFinder s(m);
+    int g = index(side - 2, side - 2, side);
+
+    EXPECT_EQ(g, s.getGoal());
+}
+
+TEST(testingMazeSolver, testSolvingSeeded) {
+    std::vector <uint8_t> mazeDimTwoSeedZero = {0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2, 0, 0, 1, 1, 2, 0, 0, 0, 0, 0, 0};
+
+    mazeGrid m(2);
+    m.setSeed(0);
+    m.makeMaze();
+
+    pathFinder s(m);
+    s.solveMaze();
+    std::vector <uint8_t> solvedMaze = s.getMaze();
+
+    for (int i = 0, size = mazeDimTwoSeedZero.size(); i < size; i++) {
+        EXPECT_EQ(mazeDimTwoSeedZero[i], solvedMaze[i]);
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P (
+    attributes,
+    testingMaze,
+    ::testing::Values(1, 2, 3, 34, 234, 255, 99, 10)
+);
+
+INSTANTIATE_TEST_SUITE_P (
+    invalidInputs, 
+    testingInvalidMaze, 
+    ::testing::Values(-1, 0, 23232, 256, -23432)
+);
+
+INSTANTIATE_TEST_SUITE_P (
+    solving,
+    testingMazeSolver,
+    ::testing::Values(1, 2, 10, 15, 20, 50, 100, 200, 255)
+);
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
