c68ca30e10ea278e5666cc7ec6641527696a19d4
eyad-jawad <ayadmansr@gmail.com>
2026-04-25 19:18:27 +0300

    Add setSeed function to the WASM unit

--- docs/game/wasm/wasmLayer.cpp
+++ docs/game/wasm/wasmLayer.cpp
@@ -1,10 +1,10 @@
-#include "mazeGame/project/maze/mazeGenerator.h"
+#include "../../../project/maze/mazeGenerator.h"
 
 std::vector <uint8_t> maze;
 
 extern "C" {
     uint8_t * run(int dimensions) {
-        auto m = mazeGrid (dimensions);
+        mazeGrid m(dimensions);
         m.makeMaze();
         maze = m.getMaze();
         return maze.data();
@@ -12,5 +12,13 @@ extern "C" {
 
     int size(int dimensions) {
         return (2 * dimensions + 1) * (2 * dimensions + 1);
+    }
+
+    uint8_t * setSeed(int dimensions, int seed) {
+        mazeGrid m(dimensions);
+        m.setSeed(seed);
+        m.makeMaze();
+        maze = m.getMaze();
+        return maze.data();
     }
 }
